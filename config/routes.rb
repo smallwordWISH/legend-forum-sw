5b5250d8a533a0adbfe0930f8d3759df5ad47f67
smallwordWISH <word714cs@gmail.com>
2018-04-11 19:05:34 +0800

    setup controllers, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,35 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "posts#index"
+
+  resources :posts do
+    resources :replies, only: [:index, :create, :update, :destroy]
+
+    member do 
+      post :favorite
+      post :unfavorite
+      post :draft
+    end
+  end
+
+  resources :favorites, only: [:index, :create, :destroy]
+
+  resources :friendships, only: [:index, :create, :update, :destroy]
+
+  resources :views, only: [:create]
+
+  resources :users, only: [:show, :edit, :update]
+
+  namespace :admin do
+    resources :categories, except: [:show, :new, :edit]
+    resources :users, only: :index do
+
+      member do
+        post :add_admin
+      end
+    end
+    root "categories#index"
+  end
 end
